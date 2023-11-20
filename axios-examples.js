c1a58007cd6af4a82e4b3b94574ab70046245d24
Dima Smaha <fannifty@gmail.com>
2023-11-20 17:56:34 +0200

    first commit

---
+++ axios-examples.js
@@ -0,0 +1,51 @@
+const axios = require("axios");
+
+axios
+  .get("https://reqres.in/api/users?page=2")
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+axios
+  .get("https://reqres.in/api/users", {
+    params: {
+      page: 1,
+    },
+  })
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+axios
+  .post("https://reqres.in/api/users", {
+    name: "morpheus",
+    job: "leader",
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+function createUser() {
+  return axios
+    .post("https://reqres.in/api/users", {
+      name: "morpheus",
+      job: "leader",
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+createUser();
