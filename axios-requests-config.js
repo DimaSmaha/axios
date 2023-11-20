0c6990a84df2650730ea05f71e9b19110863fec6
Dima Smaha <fannifty@gmail.com>
2023-11-20 19:24:55 +0200

    upd

--- axios-requests-config.js
+++ axios-requests-config.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 
 const axiosConfig = axios.create({
   baseURL: "https://reqres.in/",
+  //   headers: {"":""}
 });
 
 const registerUserBody = { email: "eve.holt@reqres.in", password: "pistol" };
