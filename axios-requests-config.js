46d1b538ff1c7de180986729780425f23dfbf0d4
Dima Smaha <fannifty@gmail.com>
2023-11-20 19:23:09 +0200

    das

---
+++ axios-requests-config.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+
+const axiosConfig = axios.create({
+  baseURL: "https://reqres.in/",
+});
+
+const registerUserBody = { email: "eve.holt@reqres.in", password: "pistol" };
+
+function apiResponse(response) {
+  console.log("--------Response start-------------------");
+  console.log(response.data);
+  console.log(response.status);
+  console.log(response.statusText);
+  console.log("--------Response end----------------");
+}
+
+async function getListOfUsers2(userBody) {
+  const client = axiosConfig;
+  return client
+    .post("/api/register", userBody)
+    .then((response) => {
+      return apiResponse(response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+async function runRequests() {
+  await getListOfUsers2(registerUserBody);
+}
+
+runRequests();
